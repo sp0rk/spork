4a79304fc18758cfbe7a424a32d1b0234a87b0dd
Julian Jurec <julian.jurec@gmail.com>
2022-12-06 16:36:41 +0100

    3: Add EntryCard component template

---
+++ designSystem/src/main/java/io/github/sp0rk/designSystem/component/Entry.kt
@@ -0,0 +1,6 @@
+package io.github.sp0rk.designSystem.component
+
+data class Entry(
+    val title: String,
+    val content: String
+)
--- designSystem/src/main/java/io/github/sp0rk/designSystem/component/Greeting.kt
+++ designSystem/src/main/java/io/github/sp0rk/designSystem/component/Greeting.kt
@@ -1,19 +1,96 @@
 package io.github.sp0rk.designSystem.component
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.sp0rk.designSystem.theme.SporkTheme
+import kotlin.random.Random
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun EntryCard(entry: Entry) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(android.R.drawable.ic_menu_preferences),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondaryVariant, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = entry.title,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
+                Text(
+                    text = entry.content,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Entry>) {
+    LazyColumn {
+        messages.map { item { EntryCard(it) } }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun PreviewConversation() {
     SporkTheme {
-        Greeting("Android")
+        Conversation(sampleData)
+    }
+}
+
+val sampleData = buildList {
+    repeat(10) {
+        add(Entry(Random.nextInt(0, 1000).toString(), Random.nextInt(0, 1000000).toString()))
     }
 }
